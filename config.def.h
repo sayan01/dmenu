4f8281e6caa5d305978d119fe4574f664e6ad122
Sayan Ghosh <sgic2001@gmail.com>
2020-11-15 21:13:27 +0530

    Updated config.def.h

--- config.def.h
+++ config.def.h
@@ -5,22 +5,22 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Hack:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#ffffff", "#263238" },
+	[SchemeSel] = { "#eeeeee", "#0d47a1" },
+	[SchemeSelHighlight] = { "#e91e63", "#0d47a1" },
+	[SchemeNormHighlight] = { "#ffc978", "#263238" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 20;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
+static unsigned int min_lineheight = 16;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -29,4 +29,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 10;
