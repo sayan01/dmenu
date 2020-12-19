949f8a729381edca7e133d7530abb15300fadf49
Sayan Ghosh <sgic2001@gmail.com>
2020-12-19 17:58:00 +0530

    Added Noto Color Emoji font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,9 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:size=16:antialias=true:autohint=true"
+	"Bitstream Vera Sans:size=16",
+	"Noto Color Emoji:size=16",
+	"fontawesome:size=16",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
